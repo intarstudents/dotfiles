495684da184375a6ef6d59c77bb37faced348273
Intars Students <the.mobix@gmail.com>
2022-05-31 11:11:04 +0300

    On Linux Mint 20.3 touch didn't trigger close_write so added attrib metadata change to

--- xkb/linux/udev.sh
+++ xkb/linux/udev.sh
@@ -4,7 +4,7 @@ BASEDIR=$(/usr/bin/dirname $(/usr/bin/realpath -s $0))
 TRG_FILE="/tmp/xkb-udev"
 
 touch $TRG_FILE
-while inotifywait -e close_write $TRG_FILE;
+while inotifywait -e close_write -e attrib $TRG_FILE;
 do
 	sleep 1
 	$BASEDIR/xmodmap.sh
